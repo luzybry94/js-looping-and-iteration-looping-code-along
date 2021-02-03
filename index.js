5e1ba43885df803dc9a457b79ae500d2dfc6bc97
luzybry94 <luzybry94@gmail.com>
2021-02-03 17:00:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let newArray = []
+    for(let i = 0; i < array.length; i++) {
+      newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray
+}
+
+function countDown(int) {
+    let i = int;
+    while(int >= 0) {
+        console.log(int);
+        int--;
+    }
+}
